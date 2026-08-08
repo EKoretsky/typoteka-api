c7cb6547fffb670d85e3d458509162ee8ef18fbf
Eugene Koretsky <E.Koretsky@outlook.com>
2026-08-09 01:13:55 +0300

    init project

---
+++ src/main/kotlin/com/typoteka/api/TypotekaApiApplication.kt
@@ -0,0 +1,11 @@
+package com.typoteka.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TypotekaApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TypotekaApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/typoteka/api/TypotekaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.typoteka.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TypotekaApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
